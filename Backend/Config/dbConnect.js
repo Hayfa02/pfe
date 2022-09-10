d3f9d1404174bf87da548dcc60eb60bfeb4b412d
hayfa_ou <ounissi.hayfaa@gmail.com>
2022-09-10 15:51:46 +0200

    config db

--- Backend/Config/dbConnect.js
+++ Backend/Config/dbConnect.js
@@ -1,4 +1,4 @@
-/**const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/PFE').then(() => {
@@ -7,43 +7,4 @@ mongoose.connect('mongodb://localhost:27017/PFE').then(() => {
   .catch((err) => {
     console.log("Could not connect to the database. Error...");
     process.exit();
-  });**/
-
-
-const mongoose = require("mongoose");
-
-// ...
-
-/**const start = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb://root:password@mongo-backend:27017/PFE',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-    console.log("Successfully connected to the database");
-  } catch (error) {
-    console.log("Could not connect to the database. Error...");
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-start();**/
-
-/**mongoose.connect(
-  'mongodb://root:password@mongo-backend:27017/PFE',
-{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).
-  catch(error => handleError(error));**/
-
-// Or:
-try {
-  mongoose.connect('mongodb://root:password@mongo-backend:27017/PFE');
-} catch (error) {
-  handleError(error);
-}
+  });
