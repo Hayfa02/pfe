9c7fdb949833f06ca3fe1423f6b2e08397a321d9
hayfa_ou <ounissi.hayfaa@gmail.com>
2022-09-10 18:25:08 +0200

    config db

--- Backend/Config/dbConnect.js
+++ Backend/Config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/PFE', {user:'root', password:'password'},{auth: {authSource:'PFE'}}).then(() => {
+mongoose.Promise = global.Promise;;
+mongoose.connectted('mongodb://localhost:27017/PFE', {user:'root', password:'password'},{auth: {authSource:'PFE'}}).then(() => {
   console.log("Successfully connected to the database");
 })
   .catch((err) => {
