4b01be749df29e61018d481fc51fed17dad907b5
hayfa_ou <ounissi.hayfaa@gmail.com>
2022-09-11 13:25:32 +0200

    config db

--- Backend/Config/dbConnect.js
+++ Backend/Config/dbConnect.js
@@ -1,23 +1,13 @@
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+// The default url used to connect to MongoDB is mongodb://[service-name-decribed-in-the-compose-file]:/27017
+// In this case, the service name is "database"
+const url = 'mongodb://root:password@mongo-backend:27017/PFE';
 
-const server = 'mongo-backend:27017'; // REPLACE WITH YOUR OWN SERVER
-//const server1= 'localhost:27017';
-const database = 'PFE';          // REPLACE WITH YOUR OWN DB NAME
-const user = 'root';
-const password = 'password'
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://root:password@mongo-backend:27017/PFE', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            //useFindAndModify: false,
-            //useCreateIndex: true
-        });
+// Connect to MongoDB
+MongoClient.connect(url, (err, client) => {
+  if(err){
+    throw new Error('Could not connect to the database');
+  }
 
-        console.log('MongoDB connected!!');
-    } catch (err) {
-        console.log('Failed to connect to MongoDB', err);
-    }
-};
-
-connectDB();
+  console.log('Successfully connected to the database');
+})
