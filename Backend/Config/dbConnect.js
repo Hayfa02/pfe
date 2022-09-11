f1b1adcf93348be3776ebf4455d1a817905d99a8
hayfa_ou <ounissi.hayfaa@gmail.com>
2022-09-11 12:04:03 +0200

    config db

--- Backend/Config/dbConnect.js
+++ Backend/Config/dbConnect.js
@@ -1,23 +1,15 @@
 const mongoose = require('mongoose');
 
 const server = 'mongo-backend:27017'; // REPLACE WITH YOUR OWN SERVER
-//const server1= 'localhost:27017';
 const database = 'PFE';          // REPLACE WITH YOUR OWN DB NAME
-const user = 'root';
-const password = 'password'
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://root:password@mongo-backend:27017/PFE', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            //useFindAndModify: false,
-            //useCreateIndex: true
-        });
 
-        console.log('MongoDB connected!!');
-    } catch (err) {
-        console.log('Failed to connect to MongoDB', err);
-    }
-};
-
-connectDB();
+mongoose.connect('mongodb://root:password@mongo-backend:27017/PFE', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('MongoDB connected!!');
+}).catch(err => {
+    console.log('Failed to connect to MongoDB', err);
+});
