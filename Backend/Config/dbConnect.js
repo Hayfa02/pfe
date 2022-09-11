ef25edce270bb13c174c39b5121ca0380ab232aa
hayfa_ou <ounissi.hayfaa@gmail.com>
2022-09-11 13:53:57 +0200

    config db

--- Backend/Config/dbConnect.js
+++ Backend/Config/dbConnect.js
@@ -1,13 +1,21 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const mongoose = require("mongoose");
 // The default url used to connect to MongoDB is mongodb://[service-name-decribed-in-the-compose-file]:/27017
 // In this case, the service name is "database"
-const url = 'mongodb://root:password@mongo-backend:27017/';
+//const url = 'mongodb://root:password@mongo-backend:27017/';
 
 // Connect to MongoDB
-MongoClient.connect(url, (err, client) => {
+/**MongoClient.connect(url, (err, client) => {
   if(err){
     throw new Error('Could not connect to the database');
   }
 
   console.log('Successfully connected to the database');
 })
+**/
+const _database = 'mongodb://root:password@mongo-backend:27017/PFE?authSource=admin';
+mongoose.connect(_database, {
+    useNewUrlParser: true
+})
+.then(() => console.log('Connected to MongoDB ...'))
+.catch(err => console.error('Could not connect to MongoDB:‌', err));
